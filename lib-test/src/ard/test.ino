7f2b09f4d816c53375be8b64fa262101ae6b7ea7
Aleksandur Kolev <132449044+AMKolev22@users.noreply.github.com>
2025-01-13 00:12:21 +0200

    Optimized ard files and added more functionality

--- lib-test/src/ard/test.ino
+++ lib-test/src/ard/test.ino
@@ -1,8 +1,28 @@
+bool isMoving = false; 
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); 
 }
 
 void loop() {
-  if (Serial.available())
-    Serial.write( toupper (Serial.read()) );
+  static char buffer[5];
+  if (Serial.available()) {
+    size_t len = Serial.readBytesUntil('\n', buffer, sizeof(buffer) - 1);
+    buffer[len] = '\0';
+
+    String command = String(buffer);
+    if (command == "move" && !isMoving) {
+      isMoving = true;
+      Serial.write("moving\n");
+    } 
+    else if (command == "stop" && isMoving) {
+      isMoving = false;
+      Serial.write("stopping\n");
+    }
+  }
+  delay(30);
 }
+
+
+
+
