e16bcda29e7e9446ac0ffa0ffc3b65451261f25e
Aleksandur Kolev <132449044+AMKolev22@users.noreply.github.com>
2025-01-12 17:06:11 +0200

    Update test.ino

--- lib-test/src/ard/test.ino
+++ lib-test/src/ard/test.ino
@@ -2,9 +2,7 @@ void setup() {
   Serial.begin(115200);
 }
 
-// Loop forever
 void loop() {
-  // If serial data is pending, read, capitalize and write the character
   if (Serial.available())
     Serial.write( toupper (Serial.read()) );
 }
